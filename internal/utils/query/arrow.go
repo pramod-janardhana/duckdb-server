3448a1e36191fe1f744d378fb167dcf6b8581df7
Pramod J <pramod.athreya.0098@gmail.com>
2024-07-17 18:02:24 +0530

    write buf in ipc format

--- internal/utils/query/arrow.go
+++ internal/utils/query/arrow.go
@@ -1,12 +1,14 @@
 package query
 
 import (
+	"bytes"
 	"context"
 	querybuilder "duckdb-server/internal/query_builder"
 	pb "duckdb-server/internal/services/grpc_arrow/data_transform"
 	"log"
 
 	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/ipc"
 )
 
 func ArrowTransformV2(qb *querybuilder.DuckDBArrowQueryBuilder, query string) (*pb.QueryOut, error) {
@@ -52,12 +54,15 @@ func GetChunk(rows array.RecordReader, size int64) (*pb.QueryOut, error) {
 	var count int64 = 0
 	for count < size && rows.Next() {
 		record := rows.Record()
-		data, err := record.MarshalJSON()
-		if err != nil {
+
+		var buf []byte
+		var bufReader = bytes.NewBuffer(buf)
+		writer := ipc.NewWriter(bufReader, ipc.WithSchema(record.Schema()))
+		if err := writer.Write(record); err != nil {
 			log.Printf("GetChunk: Error marshaling record, err: %s\n", err.Error())
 		}
 
-		queryOut.Data = append(queryOut.Data, data)
+		queryOut.Data = append(queryOut.Data, bufReader.Bytes())
 		count += record.NumRows()
 		record.Release()
 	}
